9c3bab6d47d10895f4501aa235463f0bc16c8021
vshevch <vlad.shev@gmail.com>
2018-09-25 10:07:16 -0400

    Update MapToggler.py

--- MapToggler.py
+++ MapToggler.py
@@ -1,5 +1,8 @@
 from Spotfire.Dxp.Application.Visuals import *
 
+# You need to add script parameters to make this work.
+# In 'Edit Script' window, go to 'Script parameters' section, click 'Add...', and select your visualisation
+
 v = viz.As[VisualContent]()
 for l in v.Layers:
   if l.Title == “Swedish”:
