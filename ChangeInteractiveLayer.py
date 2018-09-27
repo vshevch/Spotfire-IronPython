50d299fd2c4d99ac0b063d52c3237eabe4b256fc
vshevch <vlad.shev@gmail.com>
2018-09-27 16:44:45 -0400

    Update ChangeInteractiveLayer.py

--- ChangeInteractiveLayer.py
+++ ChangeInteractiveLayer.py
@@ -1,10 +1,27 @@
 # You need to add a script parameter to make this work.
 # In 'Edit Script' window, go to 'Script parameters' section, click 'Add...', and select the Visualisation and Document Property (optional, used for dropdown menu) you want to use
- 
-from Spotfire.Dxp.Application.Visuals import *
 
-viz = MapViz.As[VisualContent]()
+from Spotfire.Dxp.Data import * 		#Required for clearMarkings function
+from Spotfire.Dxp.Application.Visuals import *  #Required for viz.Layer toggling
+
+# function to clear data Markings
+def clearMarkings(desLayer):
+	for dataTable in Document.Data.Tables:
+		if dataTable.Name == desLayer:
+			for marking in Document.Data.Markings:
+				rows = RowSelection(IndexSet(dataTable.RowCount, False))
+				marking.SetSelection(rows, dataTable)
+
+# determines which layer to clear				
+if LayerName == 'FSCs':
+	desiredLayer = 'StatsCan_FSA'
+elif LayerName == 'StatsCan_FSA':
+	desiredLayer = 'FSCs'
+
+# layer toggeling
+viz = MapViz.As[VisualContent]() # externally defined Spotfire Script Parameter
 
 for I in viz.Layers:
-	if I.Title == LayerName: #LayerName is a DocumentProperty, but this script works with static variable as well
+	if I.Title == LayerName:
 		viz.InteractiveLayerReference = I
+	clearMarkings(desiredLayer)
