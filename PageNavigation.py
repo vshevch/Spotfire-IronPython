8667126e4d2a8bbdd77d4f2e6d8288fb1a54d3bb
vshevch <vlad.shev@gmail.com>
2018-09-27 14:16:48 -0400

    Create PageNavigation.py

---
+++ PageNavigation.py
@@ -0,0 +1,43 @@
+# Source Material: http://spotfired.blogspot.com/2014/08/toggle-page-navigation.html
+
+#Navigate to the first page
+Document.ActivePageReference = Document.Pages[0]
+
+#Loop Document Pages
+for p in Document.Pages: print p.Title
+
+#Toogle Page Navigation
+if Document.Pages.NavigationMode == Document.Pages.NavigationMode.Tabs:
+   Document.Pages.NavigationMode = Document.Pages.NavigationMode.Links
+elif Document.Pages.NavigationMode == Document.Pages.NavigationMode.Links:
+   Document.Pages.NavigationMode = Document.Pages.NavigationMode.None
+elif Document.Pages.NavigationMode == Document.Pages.NavigationMode.None:
+   Document.Pages.NavigationMode = Document.Pages.NavigationMode.Tabs
+
+#Go to First page
+Document.ActivePageReference = Document.Pages[0].Title
+
+#Delete First page
+Document.Pages[0].Remove()
+
+
+#go to last page
+print Document.Pages[(Document.Pages.Count-1)].Title
+
+#Delete last page
+Document.ActivePageReference = Document.Pages.RemoveAt(Document.Pages.Count-1)
+
+
+#Delete all pages!
+print Document.Pages.Clear()
+
+#Create a page
+p = Document.Pages.AddNew()
+p.Title = "New Page"
+
+#current page number
+print Document.Pages.IndexOf(Document.ActivePageReference)
+
+#total pages
+
+print Document.Pages.Count
