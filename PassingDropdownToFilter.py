4d4ad178f23f815da00f5e91bd7daec16e97f1d1
vshevch <vlad.shev@gmail.com>
2018-11-14 14:59:50 -0500

    Create PassingDropdownToFilter.py

---
+++ PassingDropdownToFilter.py
@@ -0,0 +1,15 @@
+import Spotfire.Dxp.Application.Filters as filters
+
+myPanel = Document.ActivePageReference.FilterPanel
+# Select Data Table to Use
+BDG_Filter = myPanel.TableGroups[0]
+# Select which Filter from Data Table to Use
+myFilter = BDG_Filter.GetFilter("Month, Year of Create Month")
+# Declare filter's type (ex. ItemFilter, ListBoxFilter, etc.)
+lbFilter = myFilter.FilterReference.As[filters.ItemFilter]()
+
+# Extract Date Value from DropDown Menu
+date_value = Document.Properties["Filter2"]
+
+# Pass DropDown Selection to the Filter
+lbFilter.Value = date_value
