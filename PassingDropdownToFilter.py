986b290712aca16e1a8c0ba440a08428fbda72b4
vshevch <vlad.shev@gmail.com>
2019-03-11 14:08:59 -0400

    Update PassingDropdownToFilter.py

--- PassingDropdownToFilter.py
+++ PassingDropdownToFilter.py
@@ -3,15 +3,40 @@
 import Spotfire.Dxp.Application.Filters as filters
 
 myPanel = Document.ActivePageReference.FilterPanel
-# Select Data Table to Use
-BDG_Filter = myPanel.TableGroups[0]
+# Select Data Table to Use # use print(myPanel.TableGroups[0].Context) to check name
+BDG_Filter = myPanel.TableGroups[0] 
 # Select which Filter from Data Table to Use
 myFilter = BDG_Filter.GetFilter("Month, Year of Create Month")
 # Declare filter's type (ex. ItemFilter, ListBoxFilter, etc.)
 lbFilter = myFilter.FilterReference.As[filters.ItemFilter]()
 
-# Extract Date Value from DropDown Menu
-date_value = Document.Properties["Filter2"]
+# Extract Date Value from DropDown Menu (The "Name" of "Select property" from Property Control)
+date_value = Document.Properties["DateFilter"]
 
 # Pass DropDown Selection to the Filter
 lbFilter.Value = date_value
+
+
+# Filter #2
+
+import Spotfire.Dxp.Application.Filters as filters
+#import Spotfire.Dxp.Application.Filters.ListBoxFilter
+#from Spotfire.Dxp.Application.Filters import FilterTypeIdentifiers
+from Spotfire.Dxp.Data import DataPropertyClass
+#from System import String
+
+myPanel = Document.ActivePageReference.FilterPanel
+myFilter= myPanel.TableGroups[0].GetFilter("Rep Name")
+lbFilter = myFilter.FilterReference.As[filters.ListBoxFilter]()
+lbFilter.IncludeAllValues=False
+strVals = Document.Properties["RepNameBDG"]
+
+if strVals == None:
+	lbFilter.Reset()
+else:
+	lbFilter.SetSelection(strVals)
+
+#if strVals!=String.Empty:
+#  lbFilter.SetSelection(strVals)
+#else:
+#  lbFilter.Reset()
