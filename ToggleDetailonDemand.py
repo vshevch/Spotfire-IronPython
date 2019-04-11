507040bddcb0e46ce3c9c6c947e095444f4e05ea
vshevch <vlad.shev@gmail.com>
2019-04-11 14:55:45 -0400

    Create ToggleDetailonDemand.py

---
+++ ToggleDetailonDemand.py
@@ -0,0 +1,4 @@
+if (Document.ActivePageReference.DetailsOnDemandPanel.Visible  == False):
+	Document.ActivePageReference.DetailsOnDemandPanel.Visible = True
+else:
+	Document.ActivePageReference.DetailsOnDemandPanel.Visible = False
