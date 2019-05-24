0b113aa7945f2afd2eb1dc3576d791ff08a2baf7
vshevch <vlad.shev@gmail.com>
2019-05-24 13:44:56 -0400

    Create HTMLButtonToggle.py

---
+++ HTMLButtonToggle.py
@@ -0,0 +1,18 @@
+from Spotfire.Dxp.Application.Visuals import HtmlTextArea
+for vis in Document.ActivePageReference.Visuals:
+	if vis.Title == 'nbuttons':
+	
+		# Each line finds and replaces a part of the visualization's HTML code
+		HTMLcode = vis.As[HtmlTextArea]().HtmlContent.replace('id=lwuc style="FLOAT: left; DISPLAY: visible"','id=lwuc style="FLOAT: left; DISPLAY: none"') 
+		HTMLcode = HTMLcode.replace('id=lwc style="FLOAT: left; DISPLAY: none"','id=lwc style="FLOAT: left; DISPLAY: visible"')
+
+		HTMLcode = HTMLcode.replace('id=fwuc style="FLOAT: left; DISPLAY: none"','id=fwuc style="FLOAT: left; DISPLAY: visible"') 
+		HTMLcode = HTMLcode.replace('id=fwc style="FLOAT: left; DISPLAY: visible"','id=fwc style="FLOAT: left; DISPLAY: none"')
+
+		HTMLcode = HTMLcode.replace('id=nfyuc style="FLOAT: left; DISPLAY: none"','id=nfyuc style="FLOAT: left; DISPLAY: visible"') 
+		HTMLcode = HTMLcode.replace('id=nfyc style="FLOAT: left; DISPLAY: visible"','id=nfyc style="FLOAT: left; DISPLAY: none"')
+
+		HTMLcode = HTMLcode.replace('id=nauc style="FLOAT: left; DISPLAY: none"','id=nauc style="FLOAT: left; DISPLAY: visible"') 
+		HTMLcode = HTMLcode.replace('id=nac style="FLOAT: left; DISPLAY: visible"','id=nac style="FLOAT: left; DISPLAY: none"')
+
+		vis.As[HtmlTextArea]().HtmlContent = HTMLcode
