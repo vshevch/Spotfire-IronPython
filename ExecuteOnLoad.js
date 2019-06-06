8e06075c582a13cefde8a0253ce115351cfecdf3
vshevch <vlad.shev@gmail.com>
2019-06-06 15:02:54 -0400

    Create ExecuteOnLoad.js

---
+++ ExecuteOnLoad.js
@@ -0,0 +1,8 @@
+// Post about executing Python script
+// https://spotfired.blogspot.com/2016/10/trigger-script-when-clicking-on-tab.html
+
+window.location.href = "http://www.google.ca"; // Open in the same window
+
+window.open("https://www.w3schools.com", '_blank'); // Open in a new window
+
+// add these codes to TextArea JavaScript
