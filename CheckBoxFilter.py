e0669163f6736b5aafa7a51b5f9006fc77c3252f
vshevch <vlad.shev@gmail.com>
2019-07-11 16:58:52 -0400

    Update CheckBoxFilter.py

--- CheckBoxFilter.py
+++ CheckBoxFilter.py
@@ -14,3 +14,21 @@ for tableGroup in filterPanel.TableGroups:
     for checkedValues in cbFilter.Values:
      cbFilter.Uncheck(checkedValues)
      
+# Check Box Filter
+
+import Spotfire.Dxp.Application.Filters as filters
+#import Spotfire.Dxp.Application.Filters.ListBoxFilter
+#from Spotfire.Dxp.Application.Filters import FilterTypeIdentifiers
+from Spotfire.Dxp.Data import DataPropertyClass
+#from System import String
+
+myPanel = Document.ActivePageReference.FilterPanel
+# Here you can insert Select Filtering Scheme python code [see the code in the folder]
+myFilter= myPanel.TableGroups[0].GetFilter("Fiscal YTD Flag")
+lbFilter = myFilter.FilterReference.As[filters.CheckBoxFilter]()
+
+# Available Options
+lbFilter.Reset() # Resets the filter (includes all)
+lbFilter.Check("Y")
+lbFilter.Uncheck("Y")
+lbFilter.IncludeEmpty=False
